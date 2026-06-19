4a37fa7b749c431f5d91063eea796f5e9608e6c2
Noor_fatima <noorfatima9412@gmail.com>
2026-06-19 15:26:31 +0500

    chnage

--- backend/app/models/sale.py
+++ backend/app/models/sale.py
@@ -5,8 +5,4 @@ from app.database.database import Base
 class Sale(Base):
     __tablename__ = "sales"
 
-    id = Column(Integer, primary_key=True, index=True)
-    medicine_name = Column(String, nullable=False)
-    quantity = Column(Integer, nullable=False)
-    total_price = Column(Float, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+  
